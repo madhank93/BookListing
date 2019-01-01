567b2eb5fa55efc02c89768db314b6a1d544c695
madhank93 <maddymadhan946@gmail.com>
2019-01-01 17:12:46 +0530

    Adding loader class

--- app/src/main/java/madhank93/android/in/booklisting/MainActivity.java
+++ app/src/main/java/madhank93/android/in/booklisting/MainActivity.java
@@ -1,13 +1,72 @@
 package madhank93.android.in.booklisting;
 
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<List<Book>> {
+
+    /** URL for earthquake data from the USGS dataset */
+    private static final String BOOK_REQUEST_URL =
+            "https://www.googleapis.com/books/v1/volumes?q=";
+    /**
+     * Constant value for the earthquake loader ID. We can choose any integer.
+     * This really only comes into play if you're using multiple loaders.
+     */
+    private static final int BOOKLIST_LOADER_ID = 1;
+    private String searchParams;
+    private static final String LOG_TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView editTitle = (TextView) findViewById(R.id.search_title);
+        searchParams = editTitle.getText().toString();
+
+        Button searchButton = (Button) findViewById(R.id.search_button);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get a reference to the LoaderManager, in order to interact with loaders.
+                LoaderManager loaderManager = getLoaderManager();
+
+                // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+                // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+                // because this activity implements the LoaderCallbacks interface).
+                loaderManager.initLoader(BOOKLIST_LOADER_ID, null, MainActivity.this);
+            }
+        });
+
+
+
+    }
+
+
+    @Override
+    public Loader<List<Book>> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Book>> loader, List<Book> data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Book>> loader) {
+
     }
 }
---
+++ app/src/main/java/madhank93/android/in/booklisting/QueryUtils.java
@@ -0,0 +1,96 @@
+package madhank93.android.in.booklisting;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+public class QueryUtils {
+
+    /** Tag for the log messages */
+    private static final String LOG_TAG = QueryUtils.class.getSimpleName();
+
+    /**
+     * Create a private constructor because no one should ever create a {@link QueryUtils} object.
+     * This class is only meant to hold static variables and methods, which can be accessed
+     * directly from the class name QueryUtils (and an object instance of QueryUtils is not needed).
+     */
+    private QueryUtils() {
+    }
+
+    /**
+     * Query the USGS dataset and return a list of {@link Book} objects.
+     */
+    public static List<Book> fetchEarthquakeData(String requestUrl) {
+        // Create URL object
+        URL url = createUrl(requestUrl);
+
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = null;
+        jsonResponse = makeHttpRequest(url);
+
+        // Extract relevant fields from the JSON response and create a list of {@link Earthquake}s
+        List<Book> books = extractFeatureFromJson(jsonResponse);
+
+        // Return the list of {@link Earthquake}s
+        return books;
+    }
+
+    private static String makeHttpRequest(URL url) {
+
+        String jsonResponse = "";
+
+        // If the URL is null, then return early.
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // Closing the input stream could throw an IOException, which is why
+                // the makeHttpRequest(URL url) method signature specifies than an IOException
+                // could be thrown.
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+
+    }
+
+    private static URL createUrl(String requestUrl) {
+        URL url = null;
+        try {
+            url = new URL(requestUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Problem building the URL ", e);
+        }
+        return url;
+    }
+
+}
